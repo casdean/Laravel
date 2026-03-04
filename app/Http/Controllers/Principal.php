ff20312306dad6e4a098d06b74efc05dc21e8955
casdean <meduarda6660@gmail.com>
2026-03-04 18:19:54 -0300

    uso do controller e views com bootstrap

--- app/Http/Controllers/Principal.php
+++ app/Http/Controllers/Principal.php
@@ -6,11 +6,11 @@ use Illuminate\Http\Request;
 
 class Principal extends Controller
 {
-    function principal(){
-        echo 'Página Principal';
+    function home(){
+        return view('pagina-home');
     }
-
-    
 }
+
+
 
 
---
+++ resources/views/pagina-about.blade.php
@@ -0,0 +1,1 @@
+<h1>sobre about things coisas</h1>
